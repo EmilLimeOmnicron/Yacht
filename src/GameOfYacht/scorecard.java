9c2ed82cb4cebccc7a8e429fb590fd49f4bd688a
EmilLimeOmnicron <EmilLimeOmnicron@users.noreply.github.com>
2022-08-08 15:00:08 -0700

    score template

--- src/GameOfYacht/scorecard.java
+++ src/GameOfYacht/scorecard.java
@@ -3,13 +3,47 @@ package GameOfYacht;
 import java.util.HashMap;
 
 public class scorecard {
-
-	String scoreBucket;
-	Integer scoreValue;
-	
-	public scorecard(String score, Integer value) {
+	int choice;
+	public void ScoreChoice(int choice) {
+		if(choice == 1) {
+			
+		}
 		
-	}
+		else if(choice == 2) {
+			
+		}
+		else if(choice == 3) {
+			
+		}
+		else if(choice == 4) {
+	
+		}
+		else if(choice == 5) {
+	
+		}
+		else if(choice == 6) {
+	
+		}
+		else if(choice == 7) {
+		
+		}
+		else if(choice == 8) {
+	
+		}
+		else if(choice == 9) {
+		
+		}
+		else if(choice == 10) {
+			
+		}
+		else if(choice == 11) {
+			
+		}
+		else if(choice == 12) {
+			
+		}
+		}
+
 	
 	public void UpdateScoreCard() {
 	//	scorecard.put("Ones", null);
